0ffa92b31185a548e4fe0c8603889e1e85bbbd9b
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2018-04-21 17:04:41 +0200

    modiyfied menu burger animation for new version

--- js/app.js
+++ js/app.js
@@ -91,14 +91,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuBurger = document.querySelector(".menu-burger");
   const mainNav = document.querySelector(".main-nav");
   const arrayOfNavPositions = document.querySelectorAll(".menu-list li");
-  // const first = document.querySelector(".first");
+  const first = document.querySelector(".first");
   // const second = document.querySelector(".menu-burger-line-wrapper");
-  // const smallFirst = document.querySelector(".small-first");
-  // const smallSecond = document.querySelector(".small-second");
-  // const third = document.querySelector(".third");
+  const smallFirst = document.querySelector(".small-first");
+  const smallSecond = document.querySelector(".small-second");
+  const third = document.querySelector(".third");
 
   arrayOfNavPositions.forEach(e => {
     e.addEventListener("click", () => {
+      setTimeout(() => {
+        mainNav.classList.remove("main-nav-after");
+        first.id = "rotate-counter-left";
+        smallFirst.id = "rotate-counter-downward-right";
+        smallSecond.id = "rotate-counter-downward-left";
+        third.id = "rotate-counter-right";
+      }, 250);
       mainNav.id = "shorten-mobile";
       arrayOfNavPositions.forEach(e => {
         e.id = "bounce-back-mobile";
@@ -114,24 +121,25 @@ document.addEventListener("DOMContentLoaded", () => {
         });
         setTimeout(() => {
           mainNav.id = "shorten-mobile";
+          setTimeout(() => {
+            mainNav.classList.remove("main-nav-after");
+          }, 250);
         }, 250);
-        // mainNav.id = "shorten-mobile";
-        // first.id = "rotate-counter-left";
-        // second.id = "move-down";
-        // smallFirst.id = "rotate-counter-downward-right";
-        // smallSecond.id = "rotate-counter-downward-left";
-        // third.id = "rotate-counter-right";
+        first.id = "rotate-counter-left";
+        smallFirst.id = "rotate-counter-downward-right";
+        smallSecond.id = "rotate-counter-downward-left";
+        third.id = "rotate-counter-right";
         break;
       default:
         arrayOfNavPositions.forEach(e => {
           e.id = "bounce-mobile";
         });
         mainNav.id = "elongate-mobile";
-      // first.id = "rotate-left";
-      // second.id = "move-up";
-      // smallFirst.id = "rotate-downward-right";
-      // smallSecond.id = "rotate-downward-left";
-      // third.id = "rotate-right";
+        mainNav.classList.add("main-nav-after");
+        first.id = "rotate-left";
+        smallFirst.id = "rotate-downward-right";
+        smallSecond.id = "rotate-downward-left";
+        third.id = "rotate-right";
     }
   });
 
